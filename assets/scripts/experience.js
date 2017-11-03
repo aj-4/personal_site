cc5319dd324ebedbfdfa578f2b6f14c2d78cd8c6
Aaron Pietsch <apietsch4@gmail.com>
2017-11-02 16:17:39 -1000

    update all pages, add some images

---
+++ assets/scripts/experience.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.hidden').slideDown(600);
+	
+	//trying to make icon toggle per each job on hover
+	
+	// $('.job').hover(function(){
+	// 	$(this).closest('.job').find('job-info img').hide();
+	// },
+	// function(){
+	// 	$(this).closest('.job').find('job-info img').show();
+	// });
+
+	});
