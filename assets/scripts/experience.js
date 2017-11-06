68986663bb08ff42d7bfe40727d89086921b274f
Aaron Pietsch <apietsch4@gmail.com>
2017-11-05 18:20:53 -1000

    add features to exp page, descriptions

--- assets/scripts/experience.js
+++ assets/scripts/experience.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
 	$('.hidden').slideDown(600);
 	
 	//trying to make icon toggle per each job on hover
-	
-	// $('.job').hover(function(){
-	// 	$(this).closest('.job').find('job-info img').hide();
-	// },
-	// function(){
-	// 	$(this).closest('.job').find('job-info img').show();
-	// });
+
+	$('.job').hover(function(){
+		$(this).find('h2').css('color','black');
+		$(this).find('img').css('opacity','1');
+		
+	},
+	function(){
+		$(this).find('h2').css('color','#296977');
+		$(this).find('img').css('opacity','.85');
+	});
+
+	$('.j-tog').on('click', function(){
+		$(this).find('.j-desc').slideToggle('fast');
+	});
 
 	});
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	
+	$('.hero h2').css('color','black');
 });
